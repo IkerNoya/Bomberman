990b14b24488d29da70b0b90ff73dcee7e040b50
Iker <ikernoya4@gmail.com>
2020-05-16 12:22:04 -0300

    Created level layout and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Vector3 movement;
+    private void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        movement = new Vector3(horizontal, 0, vertical) * speed;
+        transform.position += movement * Time.deltaTime;
+    }
+}
