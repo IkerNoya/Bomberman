b0644de6173792d07b9ad075e025a0753d073586
Iker <ikernoya4@gmail.com>
2020-05-16 17:54:45 -0300

    Created randomly generated walls to the level

---
+++ Assets/Scripts/GridSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+    public GameObject DestructibleWall;
+    public float gridX;
+    public float gridY;
+    public float gridSpacing = 1.0f;
+    public Vector3 gridOrigin = Vector3.zero;
+    public int wallAmmount;
+    void Update()
+    {
+        SpawnInGrid();
+    }
+    void SpawnInGrid()
+    {
+        if (wallAmmount > 0)
+        {
+            float x = 0;
+            float z = 0;
+            if (createPosX(ref x, -8, 8) == true && createPosZ(ref z, -9,9)==true)
+            {
+                Vector3 CreatedPos = new Vector3(x, 0, z) + gridOrigin;
+                Instantiate(DestructibleWall, CreatedPos, Quaternion.identity);
+                wallAmmount--;
+            }
+            Debug.Log(x);
+            Debug.Log(z);
+        }
+    }
+    bool createPosX(ref float value, int min, int max)
+    {
+        value = Random.Range(min, max);
+        if (value % 2 != 0 )
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    bool createPosZ(ref float value, int min, int max)
+    {
+        value = Random.Range(min, max);
+        if (value % 2 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
